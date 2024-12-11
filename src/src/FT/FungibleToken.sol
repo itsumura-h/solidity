7e040fefff2bc207bd4f70520ee5c4c79164cb46
itsumura-h <dumblepy@gmail.com>
2024-12-11 04:00:00 +0000

    foundry.tomlとFungibleTokenの修正
    - foundry.tomlのsrcパスを"contracts"から"src"に修正。
    - foundry.tomlにフォーマット設定を追加（tab_widthを2に設定）。
    - FungibleTokenコントラクトの名前をTestTokenに変更。
    - ドキュメント内のテスト手順を削除し、より簡潔に整理。

--- src/src/FT/FungibleToken.sol
+++ src/src/FT/FungibleToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "./libs/erc20/ERC20.sol";
 
-contract FungibleToken is ERC20 {
+contract TestToken is ERC20 {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
   function mint(address to, uint256 amount) public {
