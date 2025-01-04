14515b6cd2dd663a6932d21d85a931d6f850514f
itsumura-h <dumblepy@gmail.com>
2025-01-04 08:39:25 +0000

    rm ArchetectureExampleToken v2.bk

--- src/src/ArchitectureExampleToken/v2.bk/libs/Role/RoleEvent.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
-
-abstract contract RoleEvents {
-  event GrantAdminRole(address indexed account);
-  event GrantMinterRole(address indexed account);
-}
